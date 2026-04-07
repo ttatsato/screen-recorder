7691aa7e59f71f59e9e2b2932cab1d51e0afe621
ttatsato <t-satoh@alby.co.jp>
2026-04-07 17:08:48 +0900

    WIP: scheduled-execute-on-mac

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use anyhow::{Context, Result};
+use chrono::Local;
+use clap::Parser;
+use screenshots::Screen;
+use std::{fs, path::PathBuf, thread. time:Duration};
+
+#{derive(Parser, Debug)}
+struct Args {
+    #[arg(long, default_value_t = 60)]
+    interval_sec: u64,
+
+    #[arg(long, default_value = "./captures")]
+    output_dir: PathBuf
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    
     println!("Hello, world!");
 }
